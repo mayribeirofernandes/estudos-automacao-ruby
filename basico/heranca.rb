919fbbc8ecd88fd4c70bb06495617cb3c052efb1
May Fernandes <ammmayara@hotmail.com>
2021-06-30 18:08:42 -0300

    Métodos

--- basico/heranca.rb
+++ basico/heranca.rb
@@ -1,7 +1,22 @@
 class Automovel
+
+    # métodos self são métodos que podem ser acionados pela própria classe
+    # métodos como o "acelera" só podem ser chamados pelo objeto da classe
+    def self.tipo_cambio
+        puts "Manual"
+    end
+
     def acelera
+        combustivel
         puts "Acelerando automóvel..."
     end
+
+    # declaramos os métodos privados com "private", eles só podem ser acessados dentro da classe
+    # Em Ruby, esses metodos também são acessados pelas classes herdeiras
+    private
+        def combustivel
+            puts "Verificando Combustivel"
+        end
 end
 
 class Carro < Automovel
@@ -14,3 +29,7 @@ end
 
 fusca = Carro.new
 fusca.acelera
+
+# métodos self são métodos que são acionados pela própria classe e suas herdeiras
+Automovel.tipo_cambio
+Carro.tipo_cambio
