db6e9ac44a6388e2d27b2942f080d7725ff2a0dc
May Fernandes <ammmayara@hotmail.com>
2021-07-06 13:52:57 -0300

    Symbols

---
+++ basico/symbols.rb
@@ -0,0 +1,10 @@
+# Symbol é uma string compartilhada em todo o projeto
+:may_fernandes
+
+puts :may_fernandes
+
+# Converte pra String
+puts :may_fernandes.to_s
+
+# Converte pra Symbol
+puts "mayara_fernandes".to_sym
